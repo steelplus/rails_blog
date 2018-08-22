a863887810d29a7e111f78ef71cf45a49b90d0a1
steel <hyp.foxtail@gmail.com>
2018-08-22 23:07:26 +0900

    fix axios update csrf-token

--- app/javascript/api/axios.js
+++ app/javascript/api/axios.js
@@ -2,6 +2,7 @@
 
 import axios from 'axios';
 
+// html-meta内のcsrf-tokenを返却します
 const setToken = (params) => {
     const tags = document.getElementsByTagName("meta");
     for (let i = 0; i < tags.length; i++) {
@@ -14,27 +15,30 @@ const setToken = (params) => {
     return params;
 };
 
+// csrf-tokenを更新します
 const updateToken = (params) => {
-    const tags = document.getElementsByTagName("meta");
-    for (let i = 0; i < tags.length; i++) {
-        if (tags[i].name === "csrf-token") {
-            tags[i].content = params.authenticity_token;
-            break;
+    if (params.authenticity_token !== null && params.authenticity_token !== '') {
+        const tags = document.getElementsByTagName("meta");
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].name === "csrf-token") {
+                tags[i].content = params.authenticity_token;
+                break;
+            }
         }
     }
 };
 
 export default {
     get: (path, params = {}) => {
-        return axios.get(path, {params});
+        return axios.get(path, {params}).then(response => updateToken(response)).catch(response => updateToken(response));
     },
     post: (path, params = {}) => {
-        return axios.post(path, setToken(params));
+        return axios.post(path, setToken(params)).then(response => updateToken(response)).catch(response => updateToken(response));
     },
     put: (path, params = {}) => {
-        return axios.put(path, setToken(params));
+        return axios.put(path, setToken(params)).then(response => updateToken(response)).catch(response => updateToken(response));
     },
     delete: (path, params = {}) => {
-        return axios.delete(path, {params: setToken(params)});
+        return axios.delete(path, {params: setToken(params)}).then(response => updateToken(response)).catch(response => updateToken(response));
     },
 }
